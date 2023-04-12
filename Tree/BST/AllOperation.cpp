dc2f0918c665bb6185a27480060f2f6ca9a8c34a
Debasish Sahoo <debasish.sahoo96@gmail.com>
2023-04-12 11:13:28 +0530

    new update

--- Tree/BST/AllOperation.cpp
+++ Tree/BST/AllOperation.cpp
@@ -1,5 +1,4 @@
-#include <iostream>
-
+#include <iostream>                          
 using namespace std;
 
 // Node class for BST
--- Tree/BinaryTree/Inorder Tree Traversal/InOrderStack.cpp
+++ Tree/BinaryTree/Inorder Tree Traversal/InOrderStack.cpp
@@ -36,14 +36,6 @@ void inOrder(struct Node *root)
 
 int main()
 {
-
-    // struct Node *root = new Node(1);
-    // root->left = new Node(2);
-    // root->right = new Node(3);
-    // root->left->left = new Node(4);
-    // root->left->right = new Node(5);
-
-    // inOrder(root);
     struct Node *root = new Node(15);
     root->left = new Node(12);
     root->right = new Node(54);
